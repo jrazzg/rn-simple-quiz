205d58dc627fb0363d79d5cb2b8d6141af689da4
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-02 03:51:30 +0800

    Import questions and add randomizer

--- app/QuizScreen.tsx
+++ app/QuizScreen.tsx
@@ -1,28 +1,36 @@
+import questions from "@/assets/data/questions";
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const QuizScreen = () => {
-    const [question, setQuestion] = useState<string>('Did you get dat thing oild down?');
-    const [choices, setChoices] = useState<string[]>(['Rome', 'Manila', 'Paris', 'Oslo']);
-    const [answer, setAnswer] = useState<string>('Rome');
-    
+    const [randomNumber, setRandomNumber] = useState<number>(Math.floor(Math.random() * 30));
+    const [question, setQuestion] = useState<string>(questions[randomNumber].question);
+    const [choices, setChoices] = useState<string[]>(questions[randomNumber].choices);
+    const [answer, setAnswer] = useState<string>(questions[randomNumber].answer);
+    const [count, setCount] = useState<number>(1);
+
+    const nextQuestion = () => {
+        setCount(prev => prev + 1);
+        setRandomNumber(Math.floor(Math.random() * 30));
+        setQuestion(questions[randomNumber].question);
+        setChoices(questions[randomNumber].choices);
+        setAnswer(questions[randomNumber].answer);
+    }
+
     return (
         <View style={styles.container}>
             <View>
-                <Text>QUESTION 1</Text>
+                <Text>QUESTION {count}</Text>
                 <Text style={styles.question}>{question}</Text>
-                {/* <Text style={styles.choice}>Rome</Text>
-                <Text style={styles.choice}>Manila</Text>
-                <Text style={styles.choice}>Paris</Text>
-                <Text style={styles.choice}>Oslo</Text> */}
                 {
                     choices.map((item, index) => 
-                        <TouchableOpacity onPress={() => setAnswer(item)}><Text key={index} style={styles.choice}>{item}</Text></TouchableOpacity>
+                        <TouchableOpacity key={index} onPress={() => setAnswer(item)}><Text style={styles.choice}>{item}</Text></TouchableOpacity>
                     )
                 }
                 <Text>SELECTED ANSWER: {answer}</Text>
+                <Text>Number: {randomNumber}</Text>
             </View>
-            <TouchableOpacity><Text style={styles.button}>NEXT</Text></TouchableOpacity>
+            <TouchableOpacity onPress={nextQuestion}><Text style={styles.button}>NEXT</Text></TouchableOpacity>
         </View>
     )
 }
