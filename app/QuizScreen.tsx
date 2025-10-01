84a9a0e5fac65f1c87ce0e6081f4da4d3b22f035
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-01 18:35:21 +0800

    Add QuizScreen and ResultsScreen

---
+++ app/QuizScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const QuizScreen = () => {
+    return(
+        <View>
+            <Text>Quiz Screen</Text>
+        </View>
+    )
+}
+
+export default QuizScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Link href={'/QuizScreen'}><Text>Go to Quiz Screen</Text></Link>
+        <Link href={'/ResultsScreen'}><Text>Go to Results Screen</Text></Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+})
