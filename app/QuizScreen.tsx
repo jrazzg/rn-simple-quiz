5bdcb8c09653f446f0da8cd7e3ddd48de1a2ba64
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-01 19:55:29 +0800

    Add states and selected answer

--- app/QuizScreen.tsx
+++ app/QuizScreen.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const QuizScreen = () => {
+    const [question, setQuestion] = useState<string>('Did you get dat thing oild down?');
+    const [choices, setChoices] = useState<string[]>(['Rome', 'Manila', 'Paris', 'Oslo']);
+    const [answer, setAnswer] = useState<string>('Rome');
+    
     return (
         <View style={styles.container}>
             <View>
                 <Text>QUESTION 1</Text>
-                <Text style={styles.question}>Did you get dat ting oild down?</Text>
-                <Text style={styles.choice}>Rome</Text>
+                <Text style={styles.question}>{question}</Text>
+                {/* <Text style={styles.choice}>Rome</Text>
                 <Text style={styles.choice}>Manila</Text>
                 <Text style={styles.choice}>Paris</Text>
-                <Text style={styles.choice}>Oslo</Text>
+                <Text style={styles.choice}>Oslo</Text> */}
+                {
+                    choices.map((item, index) => 
+                        <TouchableOpacity onPress={() => setAnswer(item)}><Text key={index} style={styles.choice}>{item}</Text></TouchableOpacity>
+                    )
+                }
+                <Text>SELECTED ANSWER: {answer}</Text>
             </View>
             <TouchableOpacity><Text style={styles.button}>NEXT</Text></TouchableOpacity>
         </View>
