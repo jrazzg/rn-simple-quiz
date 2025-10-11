8c1502aa127c455ed36b674047c3829f30979ab9
jrazzg <129245648+jrazzg@users.noreply.github.com>
2025-10-12 03:47:46 +0800

    Use context to ResultsScreen

--- app/ResultsScreen.tsx
+++ app/ResultsScreen.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
+import { QuizContext } from "@/context/QuizContext";
+import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const ResultsScreen = () => {
-    return(
-        <View>
+    const quizContext = useContext(QuizContext);
+
+    if (!quizContext) throw new Error('Context not available');
+    const { answers } = quizContext
+
+    return (
+        <View style={styles.container}>
             <Text>Results Screen</Text>
+            <Text>{answers}</Text>
         </View>
     )
 }
 
 export default ResultsScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 80,
+        flex: 1,
+        margin: 20,
+    },
+})
